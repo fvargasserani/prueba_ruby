ea5472426e7423f60b2248f8abc9794091a3c5b3
Francisca <fvargasserani@gmail.com>
2020-11-05 21:28:47 -0300

    first commit final assesmment ruby

---
+++ prueba_ruby.rb
@@ -0,0 +1,47 @@
+require 'net/http'
+require 'json'
+
+def request(address, api_key)
+    url = URI(address+api_key)
+    
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true
+    
+    request = Net::HTTP::Get.new(url)
+    
+    response = https.request(request)
+    JSON.parse response.read_body
+end
+
+body = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000", "&api_key=0IKxbe4GcwTwCfJsbKzegTyslzF6NhVE1gHkgRgy&page=1")
+body = body["photos"][0..25]  
+
+def build_web_page(body)
+    html = 
+    "<html>"
+    "<title> Fotos de la API de la NASA </title>""\n"
+    "<head>""\n"
+    "</head>""\n"
+    "<body>""\n"
+    "<h1> Bienvenid@, conoce fotos tomadas por la NASA en el espacio <h1>""\n"
+    "<ul>""\n"
+    
+    body.map {|photo| html += "\t""<li><img src=" + photo["img_src"] + "></li>""\n"}    
+    
+    "</ul>""\n"
+    "</body>""\n"
+    "</html>"
+
+    File.write('NASA_photos.html', html)
+end
+
+photos = body['photos'][0..20]
+
+def photos_count(photos)
+    photos.each do |x|
+        puts x["name"]["id".reverse.size]
+    end
+end
+
+build_web_page(body)
+puts photos_count(photos)
