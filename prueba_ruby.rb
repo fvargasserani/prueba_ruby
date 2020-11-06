a54daa860cf46c47d246373b8985299f5eb0afcf
Francisca <fvargasserani@gmail.com>
2020-11-05 22:29:33 -0300

    few changes

--- prueba_ruby.rb
+++ prueba_ruby.rb
@@ -13,27 +13,29 @@ def request(address, api_key)
     JSON.parse response.read_body
 end
 
-body = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000", "&api_key=0IKxbe4GcwTwCfJsbKzegTyslzF6NhVE1gHkgRgy&page=1")
-body = body["photos"][0..25]
-
 def build_web_page(body)
-    html = 
+    photos = body["photos"]
+    
+    top = 
     "<html>\n
-    <title>Fotos de la API de la NASA</title>\n
+    <title>Fotos de la NASA</title>\n
     <head>\n
     </head>\n
     <body>\n
     <h1>Bienvenid@, conoce fotos tomadas por la NASA en el espacio<h1>\n
     <ul>\n"
     
-    body.map {|photo| html += "\t""<li><img src=\"#{photo["img_src"]}\"></li>""\n"}    
+    photos.map {|x| top += "\t""<li><img src=#{x["img_src"]} width='200px'></li>""\n"}    
     
-    html +=
+    final =
     "</ul>\n
     </body>\n
     </html>"
 
+    html = top + final
+    
     File.write('NASA_photos.html', html)
 end
 
+body = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000", "&api_key=0IKxbe4GcwTwCfJsbKzegTyslzF6NhVE1gHkgRgy&page=1")
 build_web_page(body)
